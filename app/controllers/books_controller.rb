6eb2decfb160ef418b64fa8ea47ef7ddc6e91be2
EC2 Default User <ec2-user@ip-172-31-6-10.ec2.internal>
2022-11-01 08:15:46 +0000

    [Add]css

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,11 @@
 class BooksController < ApplicationController
   def new
+    @book=Book.new
   end
 
   def index
+    @book=Book.new
+    @books=Book.all
   end
 
   def show
@@ -10,4 +13,17 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to'/top'
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'books/new'
+  get 'books/new' => 'books#new'
   get 'books/edit'
   get 'books' => 'books#index'
   get 'bokes/:id' => 'books#show'
-  get '/top' => 'homes#top'
+  get 'top' => 'homes#top'
   post 'books' => 'books#create'
-  get 'books' => 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
